d1eec957da34f7101743d25240e3501a0e3cfd0e
Sushchenok Mykhailo <78119979+QuemS@users.noreply.github.com>
2021-12-21 16:00:12 +0200

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,12 @@
 
     let winterDays = [-48, -46, 48, 27, -20, -35, 43, 4, 9, 10, 41, -46, -4, 0, -38, -49, 25, -46, -48, -23, -25, -22, 12, 38, 19, -20, 26, 4, 19, 23, 26, -41, 4, -13, -9, -11, -7, 38, 27, 41, 14, -35, -38, -44, -44, -22, -24, 29, -32, 41, 7, -25, 3, 27, -45, 10, 48, 8, -34, -49, 17, -16, 41, -11, -50, -6, -34, 20, 14, -18, 39, -28, -33, -27, -48, 40, -37, -44, 0, 46, 36, -34, -50, 8, -3, 26, 40, 10, -36, 24];
 
-    let amountFrost = 0;                                       // number of freezes homework 1
+    
     let arrPeriods = [];                                       //array with the number of frosts in days
     
     for( i = 0; i < winterDays.length - 1; i++){
         if ( (winterDays[i] >= 0 ) && (winterDays[i + 1] < 0 )) {
             let number = 0;                                   //Days counter
-            amountFrost++;
             for(let j = i + 1; j < winterDays.length ; j++){
                 if ((winterDays[j] < 0 ) ){
                     number++;
@@ -24,7 +23,7 @@
     }
 
     let maxPeriods = arrPeriods[0];                              // Maximum freezing period in days
-
+    let amountFrost = arrPeriods.length;                         // number of freezes 
     for(let item of arrPeriods){
         if (item > maxPeriods) {
             maxPeriods = item;
@@ -32,3 +31,4 @@
     }
     console.log(`There were ${amountFrost} freezes in a period of ${winterDays.length} days `);
     console.log(`Maximum freezing period ${maxPeriods} days`);
+    console.log(arrPeriods);
